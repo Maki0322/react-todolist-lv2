10fdcb2343f0abe75b78b75ce97aefb9939cb80a
maki <murasame.asa.73@gmail.com>
2022-11-02 16:28:47 +0900

    削除ボタンを押すとfirebaseのデータも削除されるように関数を追加

--- src/components/todoList/TodoList.js
+++ src/components/todoList/TodoList.js
@@ -3,12 +3,21 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import "./TodoList.css";
 import dayjs from 'dayjs';
+import { doc, deleteDoc} from "firebase/firestore";
+import { db } from "../../firebase";
 
-function TodoList({text, limit, detail}) {
+
+
+function TodoList({text, limit, detail, id}) {
   // 受け取ったlimitをJSで使えるようにtoDate()で変換
   const time = limit.toDate()
   // toDate()で変換したそのままのデータは、「Sat Nov 26 2022 00:00:00 GMT+0900 (日本標準時)」って感じで使いにくいので、使いやすいようにDay.jsで変換
   const limitTime = dayjs(time);
+  
+  // TODOリストを削除する
+  const handledeleteTodo = (e) => {
+    deleteDoc(doc(db, "todo", e));
+  };
 
   return (
     <div>
@@ -26,7 +35,9 @@ function TodoList({text, limit, detail}) {
               <option>進行中</option>
               <option>完了</option>
             </select>
-            <DeleteForeverIcon />
+            <button className='deleteTodo-button' onClick={() => handledeleteTodo(id)}>
+              <DeleteForeverIcon />
+            </button>
             <ArrowBackIosNewIcon className='todolist-details-bottun'/>
           </div>
           <p>{detail}</p>
